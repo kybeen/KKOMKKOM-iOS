41d2b596536b6dc83e23944840ce757931279e45
kybeen <rei980627@gmail.com>
2023-09-21 17:48:39 +0900

    Chapter 9. 네트워크 통신과 API

--- MovieApp/MyMovieChart2/MyMovieChart2/ListViewController.swift
+++ MovieApp/MyMovieChart2/MyMovieChart2/ListViewController.swift
@@ -11,29 +11,94 @@ import UIKit
  커스텀 클래스로 프로토타입 셀의 객체 제어하기
  */
 class ListViewController: UITableViewController {
-    // 튜플 아이템으로 구성된 데이터 세트
-    var dataset = [
-        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpg"),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpg"),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg")
-    ]
+    // 현재까지 읽어온 데이터의 페이지 정보
+    var page = 1
     
     // 테이블 뷰를 구성할 리스트 데이터
     lazy var list: [MovieVO] = {
         // list 배열 변수의 클로저가 실행되는 시점에 dataset 베열이 초기화되어 있다고 보장할 수 없기 때문에 lazy 키워드 사용
         var datalist = [MovieVO]()
-        for (title, desc, opendate, rating, thumbnail) in self.dataset {
-            let mvo = MovieVO()
-            mvo.title = title
-            mvo.description = desc
-            mvo.opendate = opendate
-            mvo.rating = rating
-            mvo.thumnail = thumbnail
-            
-            datalist.append(mvo)
-        }
         return datalist
     }()
+    
+    @IBOutlet var moreBtn: UIButton!
+    
+    // 더보기 버튼 클릭 시 호출되는 메소드
+    @IBAction func more(_ sender: Any) {
+        // 현재 페이지 값에 1을 추가한다.
+        self.page += 1
+        // 영화차트 API를 호출한다.
+        self.callMovieAPI()
+        // 데이터를 다시 읽어오도록 테이블 뷰를 갱신한다.
+        self.tableView.reloadData()
+    }
+    
+    // 뷰가 처음 메모리에 로드될 때 호출되는 메소드
+    override func viewDidLoad() {
+        // 영화차트 API를 호출한다.
+        self.callMovieAPI()
+    }
+    
+    // 영화 차트 API를 호출해주는 메소드
+    func callMovieAPI() {
+        // ① 호핀 API 호출을 위한 URI를 생성
+        let url = "http://115.68.183.178:2029/hoppin/movies?version=1&page=\(self.page)&count=10&genreId=&order=releasedateasc"
+        let apiURI: URL! = URL(string: url)
+        
+        // ② REST API를 호출
+        do {
+            let apidata = try! Data(contentsOf: apiURI)
+            
+            // ③ 데이터 전송 결과를 로그로 출력
+            let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? "데이터가 없습니다."
+            NSLog("API Result=\(log)")
+            
+            // ④ JSON 객체를 파싱하여 NSDictionary 객체로 변환
+            do {
+                let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
+                
+                // ⑤ 데이터 구조에 따라 차례대로 캐스팅하며 읽어온다.
+                let hoppin = apiDictionary["hoppin"] as! NSDictionary
+                let movies = hoppin["movies"] as! NSDictionary
+                let movie = movies["movie"] as! NSArray
+                
+                // ⑥ Iterator 처리를 하면서 API 데이터를 MovieVO 객체에 저장한다.
+                for row in movie {
+                    // 순회 상수를 NSDictionary 타입으로 캐스팅
+                    let r = row as! NSDictionary
+                    
+                    // 테이블 뷰 리스트를 구성할 데이터 형식
+                    let mvo = MovieVO()
+                    
+                    // movie 배열의 각 데이터를 mvo 상수의 속성에 대입
+                    mvo.title = r["title"] as? String
+                    mvo.description = r["genreNames"] as? String
+                    mvo.thumnail = r["thumbnailImage"] as? String
+                    mvo.detail = r["linkUrl"] as? String
+                    mvo.rating = (r["ratingAverage"] as! NSString).doubleValue
+                    
+                    // list 배열에 추가
+                    self.list.append(mvo)
+                }
+                
+                // ⑦ 전체 데이터 카운트를 얻는다.
+                let totalCount = (hoppin["totalCount"] as? NSString)!.integerValue
+                
+                // ⑧ totalCount가 읽어온 데이터 크기와 같거나 클 경우 더보기 버튼을 막는다.
+                if (self.list.count) >= totalCount {
+                    self.moreBtn.setTitle("마지막 목록입니다.", for: .normal)
+                    self.moreBtn.isEnabled = false
+                }
+            } catch {
+                // 오류처리
+                NSLog("Parse Error!!")
+            }
+        } catch {
+            NSLog("API 호출 실패!!")
+        }
+        
+        
+    }
     
     // MARK: - 생성해야 할 생의 개수를 반환하는 메소드
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -53,7 +118,14 @@ class ListViewController: UITableViewController {
         cell.desc?.text = row.description
         cell.opendate?.text = row.opendate
         cell.rating?.text = "\(row.rating!)"
-        cell.thumbnail.image = UIImage(named: row.thumnail!)
+//        cell.thumbnail.image = UIImage(named: row.thumnail!)
+        
+        // 썸네일 경로를 인자값으로 하는 URL 객체를 생성
+        let url: URL! = URL(string: row.thumnail!)
+        // 이미지를 읽어와 Data 객체에 저장
+        let imageData = try! Data(contentsOf: url)
+        // UIImage 객체를 생성하여 아울렛 변수의 image 속성에 대입
+        cell.thumbnail.image = UIImage(data: imageData)
         
         return cell
     }
@@ -61,10 +133,6 @@ class ListViewController: UITableViewController {
     // MARK: - 리스트 셀을 선택했을 때 호출되는 메소드
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         NSLog("선택된 행은 \(indexPath.row) 번째 행입니다.")
-    }
-    
-    override func viewDidLoad() {
-        
     }
 }
 
