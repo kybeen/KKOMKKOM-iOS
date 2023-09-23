95ad47daaaa7f001f54671254ed1646d17c24a7e
kybeen <rei980627@gmail.com>
2023-09-24 03:53:02 +0900

    Chapter 12. 탭 바 컨트롤러

---
+++ MovieApp/MyMovieChart2/MyMovieChart2/TheaterCell.swift
@@ -0,0 +1,17 @@
+//
+//  TheaterCell.swift
+//  MyMovieChart2
+//
+//  Created by 김영빈 on 2023/09/24.
+//
+
+import UIKit
+
+class TheaterCell: UITableViewCell {
+    // 상영관명
+    @IBOutlet var name: UILabel!
+    // 연락처
+    @IBOutlet var tel: UILabel!
+    // 주소
+    @IBOutlet var addr: UILabel!
+}
